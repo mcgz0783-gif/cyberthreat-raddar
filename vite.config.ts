2e7fefe5a7f5b98cca2c7f1efc8448c048dd47fa
mcgz0783 <mcgz0783@gmail.com>
2026-08-24 20:24:39 +0000

    feat(api): integrate news API and update content automation

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  envDir: "./.env",
   server: {
     host: "::",
     port: 8080,
